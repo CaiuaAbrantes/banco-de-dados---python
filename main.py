d7f842bf026e08fb04c848d8552a766a252af08f
Caiua <caiua777@gmail.com>
2026-02-19 12:12:14 -0300

    adicionando TABLE

--- main.py
+++ main.py
@@ -13,8 +13,17 @@ connection = pymysql.connect(
 
 with connection:
     with  connection.cursor() as cursor:
-        #SQL
-        print(cursor)
+        cursor.execute(
+            'CREATE TABLE IF NOT EXISTS customers ('
+            'id INT NOT NULL AUTO_INCREMENT, '
+            'nome VARCHAR(50) NOT NULL, '
+            'idade INT NOT NULL , '
+            'PRIMARY KEY (id)'
+            ') ' 
+            
+
+        )
+        connection.commit()
         
 
     cursor.close()
