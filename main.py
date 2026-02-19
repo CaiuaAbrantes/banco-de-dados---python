8eced903c956e62eb3b85235243be1f774c579ed
Caiua <caiua777@gmail.com>
2026-02-19 10:45:42 -0300

    iniciado o main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import pymysql #type: ignore
+import dotenv #type: ignore
+import os
+
+dotenv.load_dotenv()
+
+connection = pymysql.connect(
+    host=os.environ['MYSQL_HOST'],
+    user=os.environ['MYSQL_USER'],
+    password=os.environ['MYSQL_PASSWORD'],
+    database=os.environ['MYSQL_DATABASE'],
+)
+
+with connection:
+    with  connection.cursor() as cursor:
+        #SQL
+        print(cursor)
+        
+
+    cursor.close()
